e01672b6a3df73c2a6967024877d540591feb1e6
Shelby <shelby.harrison6@hotmail.com>
2020-03-21 16:49:02 +0000

    Getting older & unhealthier- completed

--- src/newPet.js
+++ src/newPet.js
@@ -1,10 +1,21 @@
 
 class Pet {
-    constructor(name) {
-        this.name = name;
-    }
-}
+     constructor(name) {
+         this.name = name;
+         this.age = 0;
+         this.hunger = 0;
+         this.fitness = 10;
+     }
 
-function aPet() {}
+     growUp() {
+          this.age++
+          this.hunger =+ 5;
+          this.fitness -= 3;
+     }
+ };
+ 
+ function aPet() {}
 
-module.exports = { Pet, aPet };
+
+ 
+ module.exports = { Pet, aPet };
--- test/newPet.test.js
+++ test/newPet.test.js
@@ -12,3 +12,20 @@ describe('constructor', () => {
     expect(new aPet('Fido')).toBeInstanceOf(Object);
   });
 });
+
+describe("growUp", () => {
+  it("increases the pet age by 1", () => {
+    let boris = new Pet('Boris')
+    boris.growUp();
+       expect(boris.age).toBe(1)
+  });
+});
+
+describe("growUp", () => {
+  it("increases hunger by 5 & decreases fitness by 3", () => {
+    let boris = new Pet('Boris')
+    boris.growUp();
+       expect(boris.hunger).toBe(5)
+       expect(boris.fitness).toBe(7)
+  });
+});
